b54f232768bb612a55cf9877eeaf9a8c3f006a41
Kevin Nguyen <kevinsangnguyen@gmail.com>
2016-07-12 19:21:39 -0700

    first commit

---
+++ client/controllers/dashboard_controller.js
@@ -0,0 +1,28 @@
+board.controller('DashboardController', function($scope,$location,$routeParams,PollFactory) {
+
+
+	// PollFactory.index(function(data){
+	// 	$scope.polls = data;
+	// });
+	//
+	// $scope.create_poll = function() {
+	// 	PollFactory.create($scope.new_poll,function(errors) {
+	// 		if(errors.success)
+	// 			{
+	// 				$location.path('/dashboard/');
+	// 			}
+	// 		else
+	// 			{
+	// 				$scope.errors = errors;
+	// 			}
+	// 	})
+	// }
+	//
+	// $scope.delete = function (poll){
+	// 	PollFactory.remove(poll)
+	// }
+
+
+
+
+});
